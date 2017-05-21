6f589a4e5020ae2e41d93658a8a5e49a9eb8e918
Andrew Castro <andrewcastro5@gmail.com>
2017-05-21 11:44:33 -0400

    Turn video red

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,6 +24,12 @@ function paintToCanvas() {
   
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    pixels = redEffect(pixels);
+    // put them pack
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -37,8 +43,18 @@ function takePhoto() {
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="" />`
+  link.innerHTML = `<img src="${data}" alt="Handsome" />`
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+  }
+  
+  return pixels;
 }
 
 getVideo();
